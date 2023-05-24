36fdb8215fa1c5fccde6c820d8dbf44216efec4d
SeanN <seannoh1654@yahoo.ca>
2023-05-23 19:47:33 -0700

    Added JS to remove nav bar and about me section

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 // logic for location changing circle colours
 // parallalex effect for scrolling down/up
 // parallalex effect for mouse movement on home page
+
+const nav = document.querySelector(".header-container");
+const navButton = document.querySelector(".header-element");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("hidden-nav");
+    // console.log("we went down");
+  } else {
+    nav.classList.remove("hidden-nav");
+    // console.log("we went up");
+  }
+  lastScrollY = window.scrollY;
+});
+
+navButton.addEventListener("click", () => {
+  console.log("pog");
+  setTimeout(() => {
+    console.log("pog");
+    nav.classList.remove("hidden-nav");
+  }, 2000);
+});
