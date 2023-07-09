3251e39e540e5220ee3d1e9f9a36e9d4e6688d3c
SeanN <seannoh1654@yahoo.ca>
2023-07-08 18:26:49 -0700

    Added Basic Nav Bar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,86 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faHandPaper,
+  faHome,
+  faPaperclip,
+  faSheetPlastic,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="logo-subtitle" />
+      </Link>
+
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sean-noh-310b3123a/"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+            ></FontAwesomeIcon>
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/snoh1654"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e"></FontAwesomeIcon>
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/1iFbkluP_4WIDwTCi0hPhnEO-XJiCyh54/view?usp=sharing"
+          >
+            <FontAwesomeIcon
+              icon={faSheetPlastic}
+              color="#4d4d4e"
+            ></FontAwesomeIcon>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
