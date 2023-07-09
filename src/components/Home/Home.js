43184ae37bc24174c525656be10913f0ffe7b782
SeanN <seannoh1654@yahoo.ca>
2023-07-08 18:26:57 -0700

    Added Layout and Home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi,
+          <br />
+          I'm
+          <img src={LogoTitle} alt="developer" />
+          ean
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer / JavaScript Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
