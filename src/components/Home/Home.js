3dde264d129ca0eff30d26f8fc7d145559b76676
SeanN <seannoh1654@yahoo.ca>
2023-07-09 17:56:23 -0700

    Implemented About Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,27 +2,31 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './Home.scss'
 import Logo from '../Logo/Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          Hi,
-          <br />
-          I'm
-          <img src={LogoTitle} alt="developer" />
-          ean
-          <br />
-          web developer
-        </h1>
-        <h2>Frontend Developer / JavaScript Expert</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            Hi,
+            <br />
+            I'm
+            <img src={LogoTitle} alt="developer" />
+            ean
+            <br />
+            web developer
+          </h1>
+          <h2>Frontend Developer / JavaScript Expert</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
