52a70eba1c6d3ccc61eb4f3ba5e4190c0b2df904
SeanN <seannoh1654@yahoo.ca>
2023-08-02 00:25:19 -0700

    Added Routing to Skills and Projects Page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,15 @@
 import './Projects.scss'
+import Loader from 'react-loaders'
 
 const Projects = () => {
-  return <>To be implemented</>
+  return (
+    <>
+      <div className="container projects-page">
+        <p>To be implemented</p>
+        <Loader type="pacman" />
+      </div>
+    </>
+  )
 }
 
 export default Projects
