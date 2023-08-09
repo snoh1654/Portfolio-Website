e9d5e4c327dec990c95ed4e396209e08f2ac8898
SeanN <seannoh1654@yahoo.ca>
2023-08-09 02:57:09 -0700

    Finished Implementing Projects Page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -51,6 +51,40 @@ const Projects = () => {
         ></Application>
 
         <Application
+          name="Library Search System"
+          description="Created a full stack application using Django that allows users to perform CRUD operations on a library’s book collection and search books by their title and author. Utilized Django Models and SQLite to store the book data utilizing a Many-To-Many relationship between Books and Authors.	Displayed the book information utilizing Django Template Language to create a dynamic website that updated the latest book information."
+          img1="website.png"
+          imgDescription="Search Website"
+          link="https://github.com/snoh1654/Library-Search-System"
+          skills={['Python', 'Django', 'SQLite']}
+        ></Application>
+
+        <Application
+          name="Library Search System"
+          description="Created a full stack application using Django that allows users to perform CRUD operations on a library’s book collection and search books by their title and author. Utilized Django Models and SQLite to store the book data utilizing a Many-To-Many relationship between Books and Authors.	Displayed the book information utilizing Django Template Language to create a dynamic website that updated the latest book information."
+          img1="website.png"
+          imgDescription="Search Website"
+          link="https://github.com/snoh1654/Library-Search-System"
+          skills={['Python', 'Django', 'SQLite']}
+        ></Application>
+
+        <Application
+          name="example"
+          description="example description"
+          img="logo192.png"
+          link="https://github.com/"
+          skills={['JavaScript']}
+        ></Application>
+
+        <Application
+          name="example"
+          description="example description"
+          img="logo192.png"
+          link="https://github.com/"
+          skills={['JavaScript']}
+        ></Application>
+
+        <Application
           name="example"
           description="example description"
           img="logo192.png"
